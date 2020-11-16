b1c83caab569a03f46aa3b3a52154e1d874dfcbd
lamjedGaidi <lamjed.gaidi@supcom.tn>
2020-11-16 22:10:14 +0100

    oooooo

---
+++ add_modification_to_the_data_base.php
@@ -0,0 +1,18 @@
+<?php
+
+
+include "db.php";
+$Id    = $_POST['article_id'];
+$new_article = $_POST['new_data'];
+
+$sql="UPDATE les_articles 
+SET 
+    article_data = '$new_article'
+WHERE
+     article_id=".$Id ;
+if($res = $conn->query($sql)){
+    echo 'seccessfully updated ';
+}else{
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}     
+?>
