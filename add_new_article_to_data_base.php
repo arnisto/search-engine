c0e70630ab68500ffe3691e7887dde80c4b6b740
lamjedGaidi <lamjed.gaidi@supcom.tn>
2020-11-16 21:23:25 +0100

    for mohaned

--- add_new_article_to_data_base.php
+++ add_new_article_to_data_base.php
@@ -9,10 +9,11 @@ $Author    = mysqli_real_escape_string($conn,$_POST['Author']);
 $Permition = mysqli_real_escape_string($conn,$_POST['Permition']);
 $Password  = mysqli_real_escape_string($conn,$_POST['Password']);
 $Data      = mysqli_real_escape_string($conn,trim($_POST['Data']));
+$currentTime = $_SERVER['REQUEST_TIME'];
 $test =  $_POST['Data'];
 $sql="INSERT INTO `les_articles` (`article_id`, `rang_v`, `rang_h`, `titre`, `article_obj`, `article_data`, `date_creation`, `auteur`, `permission_edit`, `article_password`)
  VALUES 
- (NULL, '0', '0', '$Titre', '$Obj', '$Data', 'current_timestamp()', '$Author', '$Permition', '$Password')";
+ (NULL, '0', '0', '$Titre', '$Obj', '$Data', '$currentTime', '$Author', '$Permition', '$Password')";
 
 if (mysqli_query($conn, $sql)) {
   echo $test ;
